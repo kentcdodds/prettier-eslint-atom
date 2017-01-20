a1b540e9beef99de14c8b88d16b71e99831558c6
Kent C. Dodds <kent@doddsfamily.us>
2017-01-20 13:01:52 -0700

    style: add format script with prettier-eslint-cli

--- package-scripts.js
+++ package-scripts.js
@@ -1,3 +1,5 @@
+/* eslint max-len:0 */
+
 module.exports = {
   scripts: {
     commit: {
@@ -10,8 +12,9 @@ module.exports = {
     },
     validate: {
       description: 'This runs several scripts to make sure things look good before committing or on clean install',
-      script: 'p-s -p lint',
+      script: 'nps format && nps lint',
     },
+    format: 'prettier-eslint lib/*.js --write',
     addContributor: {
       description: 'When new people contribute to the project, run this',
       script: 'all-contributors add',
