3cc40ebe642a37f8ea2ad7523ed19848f2591344
Kent C. Dodds <kent@doddsfamily.us>
2017-01-21 14:08:44 -0700

    I should probably just transpile this...

--- package-scripts.js
+++ package-scripts.js
@@ -1,30 +1,25 @@
 /* eslint max-len:0 */
-
+'use strict'
 module.exports = {
   scripts: {
     commit: {
       description: 'This uses commitizen to help us generate well formatted commit messages',
-      script: 'git-cz',
+      script: 'git-cz'
     },
-    lint: {
-      description: 'lint the entire project',
-      script: 'eslint .',
-    },
+    lint: {description: 'lint the entire project', script: 'eslint .'},
     validate: {
       description: 'This runs several scripts to make sure things look good before committing or on clean install',
-      script: 'nps format && nps lint',
+      script: 'nps format && nps lint'
     },
-    format: 'prettier-eslint lib/*.js --write',
+    format: 'prettier-eslint lib/*.js *.js --write',
     addContributor: {
       description: 'When new people contribute to the project, run this',
-      script: 'all-contributors add',
+      script: 'all-contributors add'
     },
     generateContributors: {
       description: 'Update the badge and contributors table',
-      script: 'all-contributors generate',
-    },
+      script: 'all-contributors generate'
+    }
   },
-  options: {
-    silent: false,
-  },
+  options: {silent: false}
 }
